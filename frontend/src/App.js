50971a3b7787837f3c8e76585f05a32d10db8270
Jake Scudder <scudsterr@gmail.com>
2021-02-26 02:28:40 -0500

    CoinList.js created
    *Functional coin display

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import CryptoList from "./components/CryptoList";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <CryptoList />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/CryptoList.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const CryptoList = () => {
+  const [coinList, setCoinList] = useState([]);
+
+  useEffect(() => {
+    if (coinList && coinList.length > 0) {
+      console.log("Coins loaded:", coinList);
+    } else {
+      fetchCoinList();
+    }
+  }, [coinList]);
+
+  const fetchCoinList = async () => {
+    await axios
+      .get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd")
+      .then((res) => setCoinList(res.data));
+  };
+
+  return (
+    <div>
+      <h2>Crypto List</h2>
+      {coinList && coinList.length > 10
+        ? coinList.slice(0, 100).map((coin) => (
+            <div className="coin-container" key={coin.id}>
+              <div className="image-container">
+                <img className="coin-image" alt={coin.name} src={coin.image} />
+              </div>
+              <h3 className="coin-list-name">{coin.name}</h3>
+              <h3 className="coin-list-symbol">{coin.symbol}</h3>
+              <h3 className="coin-list-price">${coin.current_price}</h3>
+              <br />
+            </div>
+          ))
+        : null}
+    </div>
+  );
+};
+
+export default CryptoList;
